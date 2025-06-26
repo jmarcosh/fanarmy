e44d8a483b63042df817ca7df328a812cd01a938
jmarcosh <jmarcosh@gmail.com>
2025-06-26 13:21:32 +0300

    refactoring. organizing pipelines. aggregating data

--- src/preprocess/load_sales_data.py
+++ src/preprocess/load_sales_data.py
@@ -3,6 +3,7 @@ import itertools
 import numpy as np
 import pandas as pd
 
+from src.utils.utils import weighted_mean_column
 from src.utils.varnames import MONTH_NAME_TO_NUMBER, ColNames as c
 
 
@@ -50,6 +51,9 @@ def expand_df_with_all_combinations(df, freq, grouping_cols, date_col='sale_date
 
     # Sort and return
     result = result.sort_values(by=grouping_cols + [date_col]).reset_index(drop=True)
+    
+    result = result.groupby([c.SKU, c.DESCRIPTION, c.SUPPLIER,
+                           c.LICENSE, c.PLATFORM]).filter(lambda g: g[c.UNITS].notna().any())
     return result
 
 
@@ -59,7 +63,7 @@ def load_sales_data(data_path, platform_include, supplier_exclude):
         drop=True)
     df = df.groupby([
         c.SKU, c.DESCRIPTION, c.SUPPLIER, c.LICENSE, c.PLATFORM, c.MONTH, c.YEAR
-    ])[[c.UNITS, c.SALES_MXN, c.COST]].sum().reset_index()
+    ]).agg({c.UNITS: 'sum', c.SALES_MXN: 'sum', c.COST: 'mean'}).reset_index()
     # Add ate in yyyy/mm/dd format
     for col in df.select_dtypes(include=['string', 'object']).columns:
         df[col] = df[col].astype(str).str.strip()
@@ -68,22 +72,20 @@ def load_sales_data(data_path, platform_include, supplier_exclude):
     # Expand data with months we have no sales observations
     df = expand_df_with_all_combinations(df, 'MS', [c.SKU, c.DESCRIPTION, c.SUPPLIER, c.LICENSE, c.PLATFORM],
                                             date_col=c.DATE, platform_col=c.PLATFORM)
-    if df[[c.SKU, c.DESCRIPTION, c.SUPPLIER,
-                           c.LICENSE, c.PLATFORM]].isnull().any().any():
-        raise ValueError("Missing values detected in required columns.")
-    df = df.groupby([c.SKU, c.DESCRIPTION, c.SUPPLIER,
-                           c.LICENSE, c.PLATFORM]).filter(lambda g: g[c.UNITS].notna().any())
-    df[c.MONTH_NUM] = df[c.DATE].dt.month
-    df[c.YEAR] = df[c.DATE].dt.year
     df[c.UNITS] = df[c.UNITS].fillna(0)
     df[c.SKU_PLATFORM] = df[c.SKU].astype(str) + "_" + df[c.PLATFORM].astype(str)
+    df = add_price_and_cost(df)
+    df = add_year_and_month(df)  
+    df = check_for_missing_values(df, [c.SKU, c.DESCRIPTION, c.SUPPLIER,
+           c.LICENSE, c.PLATFORM, c.PRICE])
     return df
 
 
+
 def filter_out_skus_with_non_significant_sales(df, sales_threshold):
-    data_sku_platform = df.groupby([
-        c.SKU_PLATFORM], observed=True)[[c.UNITS]].sum().reset_index()
-    sorted_sales = data_sku_platform.sort_values(by=[c.UNITS], ascending=False)
+    data_sku = df.groupby([
+        c.SKU], observed=True)[[c.UNITS]].sum().reset_index()
+    sorted_sales = data_sku.sort_values(by=[c.UNITS], ascending=False)
     sorted_sales_array = sorted_sales[c.UNITS].values
     # % of SKUs
     sku_pct = np.arange(1, len(sorted_sales_array) + 1) / len(sorted_sales_array)
@@ -91,9 +93,28 @@ def filter_out_skus_with_non_significant_sales(df, sales_threshold):
     cum_sales_pct = np.cumsum(sorted_sales_array) / sorted_sales_array.sum()
     # Find index of first value greater than the threshold
     index = np.argmax(sorted_sales_array == sales_threshold)
-    selected_skus = sorted_sales[c.SKU_PLATFORM].iloc[:index].tolist()
-    dfs = df[df[c.SKU_PLATFORM].isin(selected_skus)].reset_index(drop=True)
+    selected_skus = sorted_sales[c.SKU].iloc[:index].tolist()
+    dfs = df[df[c.SKU].isin(selected_skus)].reset_index(drop=True)
     print(f'Percentage of SKUs with sales above {sales_threshold}: {sku_pct[index]:.2%}')
     print(f'Percentage of rows kept: {len(dfs) / len(df):.2%}')
     print(f'Percentage of sales in SKUs above threshold: {cum_sales_pct[index]:.2%}')
     return dfs
+
+
+def add_year_and_month(df):
+    df[c.MONTH_NUM] = df[c.DATE].dt.month
+    df[c.YEAR] = df[c.DATE].dt.year
+    return df
+
+def add_price_and_cost(df: pd.DataFrame) -> pd.DataFrame:
+    df[c.PRICE] = df[c.SALES_MXN] / df[c.UNITS]
+    df[c.PRICE] = df[c.PRICE].fillna(weighted_mean_column(df, [c.SKU_PLATFORM], c.PRICE))
+    df[c.COST] = df[c.COST].fillna(
+        weighted_mean_column(df, [c.SKU], c.COST)
+    )
+    return df
+
+def check_for_missing_values(df: pd.DataFrame, required_columns) -> pd.DataFrame:
+    if df[required_columns].isnull().any().any():
+        raise ValueError("Missing values detected in required columns.")
+    return df
